a188c2a38c3045528b7b7a3ee5c786610ea1d67c
Erwin Heba <erwin.heba@twoupdigital.com>
2021-11-25 19:56:31 +0100

    exercise-01 - Var, let, const, operators "==", "===", NaN

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,24 @@ function myFirstFunction(a=1, b) {
 myFirstFunction();
 
 const myFirstArrowFunction = () => {}
+
+var a = 1;
+a = 2;
+
+let b = 1;
+
+b = 22;
+b = 'string';
+
+const c = 1;
+
+c = 22;
+
+
+
+== // porównanie
+=== // identyczności
+
+22 == '22'
+
+22 === '22'
