44d5dbf5f8908a9e3033aa19d122026fb39b8d74
kasiarad <89206488+kasiarad@users.noreply.github.com>
2021-11-27 09:12:24 +0000

    Exercise-02 - Subscribers Counter

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,29 @@
-function myFirstFunction(a=1, b) {
-    return a;
+//zadanie
+//wytyczne: ilosc subs.; kategorie odznaczen;
+// <100 zapis o tym jak duzo brakuje, 100 = srebro, 1000 = zloto
+// 1000 subs oraz 1000 lajkow = platyna
+
+// prompt('Exercise-02');
+
+
+const userSubs = prompt('How many subscribers do you have?');
+
+console.log(userSubs);
+
+if (userSubs == null || isNaN(parseInt(userSubs)) === true) {
+    alert('you inserted an invalid value.');
+} else {
+    if (userSubs < 100) {
+        const diff = 100 - userSubs;
+        // console.log('you are missing ' + diff + ' subs to get a silver medal.');
+        console.log(`you are missing ${diff} subs to get a silver medal.` );
+    } else if (userSubs >= 100 && userSubs < 1000) {
+        console.log(`you are having ${userSubs} subs, you get a silver medal.` );
+    } else if (userSubs >= 1000) {
+        console.log(`you are having ${userSubs} subs, you get a gold medal.` );
+    }
 }
 
-myFirstFunction();
 
-const myFirstArrowFunction = () => {}
+
+
