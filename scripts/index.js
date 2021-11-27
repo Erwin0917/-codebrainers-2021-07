854b617b4062cb14be091038c07950574d09fe93
Jarek Adamczyk <j.adamczyk042@gmail.com>
2021-11-27 12:02:39 +0100

    exercise-02 - Introduction to the loops.

--- scripts/index.js
+++ scripts/index.js
@@ -2,49 +2,45 @@
 // 10 pytań;
 // tabliczka mnożenia od 1 do 10;
 
-
-
-const firstNumber = Math.round(Math.random() * 10) + 1;
-const secondNumber = Math.round(Math.random() * 10) + 1;
+let correctAnswersCount = 0;
+const numberOfQuestions = 6;
+for (let i = 1; i <= numberOfQuestions; i ++) {
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * 10) + 1;
 // 0 - 0.9999
-console.log(firstNumber);
-console.log(secondNumber);
-// console.log(Math.round(firstNumber));
-// console.log(Math.ceil(firstNumber));
-// console.log(Math.floor(firstNumber));
+
+    console.log(firstNumber);
+    console.log(secondNumber);
 
 
+    const result = prompt(`How much is ${firstNumber} * ${secondNumber}?`);
+    const numberResult = parseInt(result);
 
-const result = prompt(`How much is ${firstNumber} * ${secondNumber}?`);
-const numberResult = parseInt(result);
-
-if (isNaN(numberResult) === true) {
-    alert('It must be a number.');
-} else {
-    const correctResult = firstNumber * secondNumber;
-    if (numberResult === correctResult) {
-        alert('Correct value.');
+    if (isNaN(numberResult) === true) {
+        alert('It must be a number.');
     } else {
-        alert('Incorrect.');
+        const correctResult = firstNumber * secondNumber;
+        if (numberResult === correctResult) {
+            console.log('Correct value.');
+            correctAnswersCount++;
+        } else {
+            console.log('Incorrect.');
+        }
     }
 }
 
+console.log(`You have ${correctAnswersCount} correct answers, you are a genius`);
+const examPassed = correctAnswersCount / numberOfQuestions;
+if (examPassed > 0.5) {
+    console.log(` you have passed this difficult test.`);
+    } else {
+    console.log('please get back to school');
+}
 
-// console.log(userSubs);
-//
-// if (userSubs == null || isNaN(parseInt(userSubs)) === true) {
-//     alert('you inserted an invalid value.');
-// } else {
-//     if (userSubs < 100) {
-//         const diff = 100 - userSubs;
-//         // console.log('you are missing ' + diff + ' subs to get a silver medal.');
-//         console.log(`you are missing ${diff} subs to get a silver medal.` );
-//     } else if (userSubs >= 100 && userSubs < 1000) {
-//         console.log(`you are having ${userSubs} subs, you get a silver medal.` );
-//     } else if (userSubs >= 1000) {
-//         console.log(`you are having ${userSubs} subs, you get a gold medal.` );
-//     }
-// }
+
+
+
+
 
 
 
