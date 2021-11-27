32d9954ff8ccda9dc2ba441cbeeac1619a6d63c7
Magdalena Wołczak <listonoszmeg@gmail.com>
2021-11-27 10:52:10 +0100

    exercise-02 - Math.random Math.floor Math.ceil simple validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,50 @@
 //zadanie
-//wytyczne: ilosc subs.; kategorie odznaczen;
-// <100 zapis o tym jak duzo brakuje, 100 = srebro, 1000 = zloto
-// 1000 subs oraz 1000 lajkow = platyna
-
-// prompt('Exercise-02');
+// 10 pytań;
+// tabliczka mnożenia od 1 do 10;
 
 
-const userSubs = prompt('How many subscribers do you have?');
 
-console.log(userSubs);
+const firstNumber = Math.round(Math.random() * 10) + 1;
+const secondNumber = Math.round(Math.random() * 10) + 1;
+// 0 - 0.9999
+console.log(firstNumber);
+console.log(secondNumber);
+// console.log(Math.round(firstNumber));
+// console.log(Math.ceil(firstNumber));
+// console.log(Math.floor(firstNumber));
 
-if (userSubs == null || isNaN(parseInt(userSubs)) === true) {
-    alert('you inserted an invalid value.');
+
+
+const result = prompt(`How much is ${firstNumber} * ${secondNumber}?`);
+const numberResult = parseInt(result);
+
+if (isNaN(numberResult) === true) {
+    alert('It must be a number.');
 } else {
-    if (userSubs < 100) {
-        const diff = 100 - userSubs;
-        // console.log('you are missing ' + diff + ' subs to get a silver medal.');
-        console.log(`you are missing ${diff} subs to get a silver medal.` );
-    } else if (userSubs >= 100 && userSubs < 1000) {
-        console.log(`you are having ${userSubs} subs, you get a silver medal.` );
-    } else if (userSubs >= 1000) {
-        console.log(`you are having ${userSubs} subs, you get a gold medal.` );
+    const correctResult = firstNumber * secondNumber;
+    if (numberResult === correctResult) {
+        alert('Correct value.');
+    } else {
+        alert('Incorrect.');
     }
 }
+
+
+// console.log(userSubs);
+//
+// if (userSubs == null || isNaN(parseInt(userSubs)) === true) {
+//     alert('you inserted an invalid value.');
+// } else {
+//     if (userSubs < 100) {
+//         const diff = 100 - userSubs;
+//         // console.log('you are missing ' + diff + ' subs to get a silver medal.');
+//         console.log(`you are missing ${diff} subs to get a silver medal.` );
+//     } else if (userSubs >= 100 && userSubs < 1000) {
+//         console.log(`you are having ${userSubs} subs, you get a silver medal.` );
+//     } else if (userSubs >= 1000) {
+//         console.log(`you are having ${userSubs} subs, you get a gold medal.` );
+//     }
+// }
 
 
 
