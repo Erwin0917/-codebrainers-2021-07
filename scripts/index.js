6b5b141266ddf0b2376b355ff165123fb7595332
Erwin Heba <erwin.heba@twoupdigital.com>
2021-11-30 20:15:51 +0100

    exercise-04 - Arrays method examples

--- scripts/index.js
+++ scripts/index.js
@@ -1,104 +1,57 @@
-// // const arr = nonEmptyArr;
 //
-// //Tworzenie tablic
-// const firstArr = new Array();
-// const emptyArr = [];
-// const nonEmptyArr = ['Janek', 'Andrzej', 'Ania'];
+// numberSelected.forEach(function (a) {
+//     ticket = ticket + a;
+// });
+// const arr = numberSelected.map(function (a) {
+//     return 2 + 2;
+// });
+// numberSelected.sort(function (a, b) {
+//     if (a - b) {
+//         return 1;
+//     }
 //
-// //Poruszanie sie po tablica
-// console.log(nonEmptyArr[0]);
-// console.log(nonEmptyArr[2]);
+//     if (b - a) {
+//         return -1;
+//     }
 //
-// //Zmiana elementu w tablicy
-// nonEmptyArr[0] = ['Zosia', 'Kasia'];
+//     if (a === b) {
+//         return 0;
+//     }
+// });
+// const arr2 = numberSelected.filter(function (a) {
+//     if(a === 45) {
+//         return true;
+//     }
+// });
 //
-// console.log(nonEmptyArr[0][1]);
+// console.log(arr2)
 //
-// //Operacje na końcu tablic
-// nonEmptyArr.push('NoweImie');
-// nonEmptyArr.pop();
+// const arr3 = numberSelected.some(function (a) {
+//     if (a === 21) {
+//         return true;
+//     }
+// });
 //
-// //Operacje na początku tablic
-// nonEmptyArr.unshift('StartEl');
-// nonEmptyArr.shift();
-//
-//
-// //Usuwanie elementów od podanego indexu z określoną ilością elementów
-// console.log('nonEmptyArr with splice', nonEmptyArr.splice(1, 2));
-// console.log('nonEmptyArr after splice', nonEmptyArr);
-//
-//
-// //Sprawdzanie czy istnieje element w tablicy, jesli istnieje to zwraca jego index jeśli nie to -1
-// console.log('nonEmptyArr after splice', nonEmptyArr.indexOf('Janek'));
+// console.log(arr3);
+
+// numberSelected.reduce();
 
 
-function getRandomNumberBetween(min = 1, max = 10){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const names = ['Janek', 'Andrzej', 'Ania', 'Adrian', 'Kamil',];
+
+
+const filterOutStringsLongerThan5 = (name) => {
+    return name.length > 5;
 }
 
-function getNumbersDrawn(){
-    let availableNo = [];
+const newArr = names.filter(filterOutStringsLongerThan5).map(name => name + '22');
 
-    for (let i = 1; i <= 49; i++) {
-        availableNo.push(i);
-    }
+const clearArr = names.filter(name => name.length > 5);
 
-    const numbersDrawn = [];
+// console.log([3 ,4 ,5].map((elem) => elem * 2))
 
-    while ( numbersDrawn.length < 6 ) {
+console.log('newArr', newArr);
 
-        let randomBallNo = getRandomNumberBetween(1, availableNo.length);
-        // console.log("randomBallNo", randomBallNo)
+const oldName = 'Ada';
 
-        const indexRandomBall = availableNo.indexOf(randomBallNo);
-        // console.log("indexRandomBall", indexRandomBall)
-
-        if (indexRandomBall !== -1){
-            availableNo.splice(indexRandomBall, 1);
-            numbersDrawn.push(randomBallNo);
-            // console.log("availableNo", availableNo);
-        }
-    }
-    return numbersDrawn;
-}
-
-function compareArray(arr1, arr2) {
-    // console.log('arr1', arr1, arr2);
-
-    if (arr1 === undefined || arr2 === undefined) {
-        return false;
-    }
-
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-
-    arr1 = arr1.sort();
-    arr2 = arr2.sort();
-    // console.log('arr1', arr1, arr2);
-
-
-    arr1 = arr1.toString();
-    arr2 = arr2.toString();
-    // console.log('arr1', arr1, arr2);
-
-    return arr1 === arr2;
-}
-
-// console.log(compareArray([8, 2, 9, 1], [22, 11, 1 , 13]))
-
-function lotteryGame() {
-    let jackpot = false;
-    let ticketsCount = 0;
-
-    const numberSelected = [1, 45, 21, 49, 37, 7];
-
-    while (jackpot === false) {
-        const numbersDraw = getNumbersDrawn();
-        jackpot = compareArray(numbersDraw, numberSelected);
-        ticketsCount++;
-    }
-
-    return ticketsCount;
-}
-console.log(lotteryGame())
+filterOutStringsLongerThan5(names);
