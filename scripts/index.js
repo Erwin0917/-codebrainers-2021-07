61ff17c31ac4d54d274f5dc51d1b0d461f0886d6
kasiarad <89206488+kasiarad@users.noreply.github.com>
2021-12-01 16:53:21 +0000

    Exercise-04 Array - Homework

--- scripts/index.js
+++ scripts/index.js
@@ -104,3 +104,45 @@
 //     return ticketsCount;
 // }
 // console.log(lotteryGame())
+
+
+
+
+// 1 - Filtrowanie -> Same imiona *
+// 2 - Filtrowanie -> Zwraca same liczby -> Zwraca podwojone wartości a*a
+// 3 - usunięcie niepoprawnych wartości (Nan, null, undefined)
+// 4 - Imiona kończące się na 'A'
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
+
+const names = ['Janek', NaN, null, undefined,  'andrzej', 'Ania', 'Adrian', 'kamil', 1 , 5, 7, "2", "9", ['abc', 'Ugh'], {name: 'Jakub', city: 'Gdynia'}, 'Ada', 'Kuba'];
+
+const removeNaN = (name) => {
+    console.log(name)
+
+    if (typeof name === "number" && isNaN(name)){
+        return false;
+    }
+    return true;
+}
+console.log(names.filter(removeNaN))
+const arrayWithoutNaN = names.filter(removeNaN)
+
+//
+//
+// const filterOutStringsLongerThan5 = (name) => {
+//     return name.length > 5;
+// }
+//
+// const newArr = names.filter(filterOutStringsLongerThan5).map(name => name + '22');
+//
+// const clearArr = names.filter(name => name.length > 5);
+//
+// // console.log([3 ,4 ,5].map((elem) => elem * 2))
+//
+// console.log('newArr', newArr);
+//
+// const oldName = 'Ada';
+//
+// filterOutStringsLongerThan5(names);
