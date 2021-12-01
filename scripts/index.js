142a0c9078bbb450fcbbc5cf6aabf1d65accc646
kasiarad <89206488+kasiarad@users.noreply.github.com>
2021-12-01 11:12:14 +0000

    Exercise-03 - Lottery Game - Homework

--- scripts/index.js
+++ scripts/index.js
@@ -32,72 +32,75 @@
 // console.log('nonEmptyArr after splice', nonEmptyArr.indexOf('Janek'));
 
 
-function getRandomNumberBetween(min = 1, max = 10){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let availableNo = [];
-for (let i = 1; i <= 49; i++){
-    availableNo.push(i);
-}
-
-
-
-function getNumbersDrawn(){
-    const numbersDrawn = [];
-
-
-    while ( numbersDrawn.length < 6 ) {
-
-        let randomBallNo = getRandomNumberBetween(1, availableNo.length);
-        // console.log("randomBallNo", randomBallNo)
-
-        const indexRandomBall = availableNo.indexOf(randomBallNo);
-
-        // console.log("indexRandomBall", indexRandomBall)
-
-        if (indexRandomBall !== -1){
-            availableNo.splice(indexRandomBall, 1);
-            numbersDrawn.push(randomBallNo);
-            // console.log("availableNo", availableNo);
-        }
-
-    }
-    return numbersDrawn;
-
-}
-
-function compareArray(arr1,arr2){
-    if (arr1 === arr2) return true;
-    if (arr1 == undefined || arr2== undefined) return false;
-    if (arr1.length !== arr2.length) return false;
-
-    arr1= arr1.sort();
-    arr2= arr2.sort();
-
-    arr1= arr1.toString();
-    arr2 =arr2.toString();
-
-    return arr1 === arr2;
-
-    //return true or false
-}
-
-
-
-function lotteryGame(){
-    let jackpot = false;
-    let ticketsCount = 0;
-
-    const numberSelected = [1, 45, 21, 49, 37, 7];
-
-    while (jackpot === false) {
-        const numbersDraw = getNumbersDrawn();
-
-        jackpot = compareArray(numbersDraw, numberSelected);
-
-        ticketsCount++;
-    }
-    return ticketsCount
-}
-console.log(lotteryGame())
+//
+// #homework1
+// function getRandomNumberBetween(min = 1, max = 10){
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+//
+// function getNumbersDrawn(){
+//     let availableNo = [];
+//
+//     for (let i = 1; i <= 49; i++) {
+//         availableNo.push(i);
+//     }
+//
+//     const numbersDrawn = [];
+//
+//     while ( numbersDrawn.length < 6 ) {
+//
+//         let randomBallNo = getRandomNumberBetween(1, availableNo.length);
+//         // console.log("randomBallNo", randomBallNo)
+//
+//         const indexRandomBall = availableNo.indexOf(randomBallNo);
+//         // console.log("indexRandomBall", indexRandomBall)
+//
+//         if (indexRandomBall !== -1){
+//             availableNo.splice(indexRandomBall, 1);
+//             numbersDrawn.push(randomBallNo);
+//             // console.log("availableNo", availableNo);
+//         }
+//     }
+//     return numbersDrawn;
+// }
+//
+// function compareArray(arr1, arr2) {
+//     // console.log('arr1', arr1, arr2);
+//
+//     if (arr1 === undefined || arr2 === undefined) {
+//         return false;
+//     }
+//
+//     if (arr1.length !== arr2.length) {
+//         return false;
+//     }
+//
+//     arr1 = arr1.sort();
+//     arr2 = arr2.sort();
+//     // console.log('arr1', arr1, arr2);
+//
+//
+//     arr1 = arr1.toString();
+//     arr2 = arr2.toString();
+//     // console.log('arr1', arr1, arr2);
+//
+//     return arr1 === arr2;
+// }
+//
+// // console.log(compareArray([8, 2, 9, 1], [22, 11, 1 , 13]))
+//
+// function lotteryGame() {
+//     let jackpot = false;
+//     let ticketsCount = 0;
+//
+//     const numberSelected = [1, 45, 21, 49, 37, 7];
+//
+//     while (jackpot === false) {
+//         const numbersDraw = getNumbersDrawn();
+//         jackpot = compareArray(numbersDraw, numberSelected);
+//         ticketsCount++;
+//     }
+//
+//     return ticketsCount;
+// }
+// console.log(lotteryGame())
