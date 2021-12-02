8fd41abe519fd7e3f4c6bec4cf995d231be4614a
kasiarad <89206488+kasiarad@users.noreply.github.com>
2021-12-02 19:58:18 +0000

    Exercise 05 - Homework Object

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,25 @@ const car = {
 };
 
 // console.log(car['color']);
+//homework - dokonczyc 3 metody (setSpeed, accelerate, slowdown) z weryfikacja:
+// current speed nie moze byc wiekszy od maximumSpeed oraz mniejszy od 0
+// musi byc zawsze "number"
+// skrot od if
+// * dorobic nowe, wedle uznania, wzorujac sie na rzeczywistych samochodach
 
 class Car {
-    constructor(color) {
+    constructor(color = 'black', maximumSpeed = 230, wheels = 4) {
         this.color = color;
-        this.maximumSpeed = 230;
-        this.numberOfWheels = 4;
+        this.maximumSpeed = maximumSpeed;
+        this.numberOfWheels = wheels;
+        this.currentSpeed = 0;
     }
 
-    setColor = function(newColor) {
+    setCurrentSpeed(currentSpeed = 0){
+        this.currentSpeed = currentSpeed;
+    }
+
+    setColor = function(newColor = 'black') {
         if (typeof newColor === 'string') {
             this.color = newColor;
             return true;
@@ -25,23 +35,41 @@ class Car {
     getColor = function () {
         return this.color;
     }
+
+    accelerate = function (howMuch) {
+        const higherSpeed = this.currentSpeed + howMuch;
+        this.setCurrentSpeed(higherSpeed);
+    }
+
+    slowdown = function (howMuch) {
+        const slowerSpeed = this.currentSpeed - howMuch;
+        this.setCurrentSpeed(slowerSpeed);
+    }
 }
 
 const bmw = new Car('black');
 const open = new Car();
 
-console.log(bmw);
-console.log(open);
+console.log(bmw.currentSpeed);
+bmw.slowdown(40);
+console.log(bmw.currentSpeed);
 
-bmw.setColor('red');
 
-console.log(bmw.getColor());
 
-const wasSetNewColor = bmw.setColor(0);
 
-if (wasSetNewColor === true) {
-    //...
-}
+//
+// console.log(bmw);
+// console.log(open);
+//
+// bmw.setColor('red');
+//
+// console.log(bmw.getColor());
+//
+// const wasSetNewColor = bmw.setColor(0);
+//
+// if (wasSetNewColor === true) {
+//     //...
+// }
 
 
 
