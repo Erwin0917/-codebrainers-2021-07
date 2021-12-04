6cb9df9401ebed9d4756e8120fb35e590cdf4621
Erwin Heba <erwin.heba@twoupdigital.com>
2021-12-04 11:20:24 +0100

    exercise-06 - OOP: method overrides, extends class

--- scripts/index.js
+++ scripts/index.js
@@ -1,96 +1,42 @@
-const car = {
-    color: 'red',
-    // maximumSpeed: 230,
-    // numberOfWheels: 4
-};
-
-// console.log(car['color']);
-//homework - dokonczyc 3 metody (setSpeed, accelerate, slowdown) z weryfikacja:
-// current speed nie moze byc wiekszy od maximumSpeed oraz mniejszy od 0
-// musi byc zawsze "number"
-// skrot od if
-// * dorobic nowe, wedle uznania, wzorujac sie na rzeczywistych samochodach
-
-class Car {
-    constructor(color = 'black', maximumSpeed, wheels) {
-        this.color = color;
-        this.maximumSpeed = this.setNumericValue(maximumSpeed, 230);
-        this.numberOfWheels = this.setNumericValue(wheels, 4);
-        this.currentSpeed = 0;
+class Person {
+    constructor() {
+        this.hitPoints = 0;
+        this.strength = 0;
     }
 
-    isNumericValue(value){
-        return typeof value === "number" && !isNaN(value);
-    }
-
-    setNumericValue(value, defaultValue = 0){
-        return this.isNumericValue(value) ? value: defaultValue;
-    }
-
-    setCurrentSpeed(currentSpeed = 0){
-        if (this.isNumericValue(currentSpeed)){
-            if (currentSpeed > this.maximumSpeed || currentSpeed < 0){
-                console.log('invalid number, it can\'t be below zero nor greater than maximum speed.');
-            }else{
-                this.currentSpeed = currentSpeed;
-                }
-        }
-        console.log('insert number');
-
-    }
-
-    setColor = function(newColor = 'black') {
-        if (typeof newColor === 'string') {
-            this.color = newColor;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    getColor = function () {
-        return this.color;
-    }
-
-    accelerate = function (howMuch) {
-        if (this.isNumericValue(howMuch)){
-            const higherSpeed = this.currentSpeed + howMuch;
-            if (higherSpeed <= this.maximumSpeed){
-                this.setCurrentSpeed(higherSpeed);
-            }
-        }
-    }
-
-    slowdown = function (howMuch) {
-        const slowerSpeed = this.currentSpeed - howMuch;
-        this.setCurrentSpeed(slowerSpeed);
+    isAlive() {
+        return this.hitPoints > 0;
     }
 }
 
-const bmw = new Car('black');
-const open = new Car();
+class Hero extends Person {
+    constructor() {
+        super();
+        this.hitPoints = 100;
+        this.strength = 20;
+    }
+}
 
-console.log(bmw.currentSpeed);
-bmw.setCurrentSpeed('-40');
-console.log(bmw.currentSpeed);
-
-
-
-
-//
-// console.log(bmw);
-// console.log(open);
-//
-// bmw.setColor('red');
-//
-// console.log(bmw.getColor());
-//
-// const wasSetNewColor = bmw.setColor(0);
-//
-// if (wasSetNewColor === true) {
-//     //...
-// }
+class Villain extends Person {
+    constructor() {
+        super();
+        this.hitPoints = 50;
+        this.strength = 20;
+    }
+}
 
 
+const hero = new Hero();
+const villain = new Villain();
 
+console.log('hero instanceof Person:', hero instanceof Hero);
+console.log('hero instanceof Villain:', hero instanceof Villain);
+
+console.log('Villain instanceof Person:', villain instanceof Hero);
+console.log('Villain instanceof Villain:', villain instanceof Villain);
+
+
+
+hero.isAlive();
+villain.isAlive();
 
