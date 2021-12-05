8ca1f76269cf9f432f8ce3c2dfb3429bdd152d2c
kasiarad <89206488+kasiarad@users.noreply.github.com>
2021-12-05 19:37:04 +0000

    Exercise 06 - Weapon Homework

--- scripts/index.js
+++ scripts/index.js
@@ -196,3 +196,111 @@ console.log(names.filter(remove4b))
 // const oldName = 'Ada';
 //
 // filterOutStringsLongerThan5(names);
+
+
+
+
+function getRandomNumberBetween(min = 1, max = 10){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// const onButtonClick = () => {
+//     console.log('Action');
+// }
+const button = document.querySelector('#button');
+
+// console.log(button);
+
+const weaponSelector = ['wand', 'knife', 'arch', 'hands', 'axe', 'sword', 'rope', 'animal']
+
+
+class Weapon {
+    constructor() {
+        this.type = weaponSelector[getRandomNumberBetween(0,weaponSelector.length-1)];
+
+    }
+
+    powerBoost() {
+        this.power = 0
+        for (let i = 0; i < weaponSelector.length; i++) {
+            console.log(weaponSelector[i])
+            if (this.type == weaponSelector[i]){
+                this.power = i
+            }
+        }
+    }
+}
+
+
+
+
+class Person {
+    constructor() {
+        this.hitPoints = 0;
+        this.strength = 0;
+    }
+
+    setWeapon(weapon) {
+        this.weapon = weapon;
+    }
+
+    isAlive() {
+        return this.hitPoints > 0;
+    }
+
+    setHitPoints(hitPoints) {
+        this.hitPoints = hitPoints >= 0 ? hitPoints : 0;
+    }
+
+
+    attack(target, power) {
+        if(!(target instanceof Person)) {
+            return false;
+        }
+        const damageFactor = Math.round(this.strength * 1/getRandomNumberBetween(1,10));
+        const hitPointsAfterAttack = target.hitPoints - power + damageFactor;
+        target.setHitPoints(hitPointsAfterAttack);
+        return target.hitPoints;
+
+    }
+}
+
+
+class Hero extends Person {
+    constructor() {
+        super();
+        this.hitPoints = 100;
+        this.strength = getRandomNumberBetween(10, 20);
+    }
+}
+
+class Villain extends Person {
+    constructor() {
+        super();
+        this.hitPoints = 100;
+        this.strength = getRandomNumberBetween(10, 20);
+    }
+}
+
+
+
+
+const battle = () => {
+    const hero = new Hero();
+    const villain = new Villain();
+
+    hero.setWeapon(new Weapon())
+
+    console.log(hero.powerBoost())
+
+    while (hero.isAlive() && villain.isAlive()) {
+        hero.attack(villain, getRandomNumberBetween(2, 35));
+        villain.attack(hero, getRandomNumberBetween(2, 31));
+
+    }
+
+    console.log(hero.hitPoints);
+    console.log(villain.hitPoints);
+}
+
+button.addEventListener('click', battle);
